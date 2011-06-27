65157f9fb184ae458b640ceda4a1d04f1b426691
Mateus Aubin <mateus.aubin@gmail.com>
2011-06-26 22:07:43 -0300

    Adicionar a URL do repositório no GitHub; Adicionar o código de debug pra ter à mão;

--- common.php
+++ common.php
@@ -1,4 +1,5 @@
 <?php
+//ini_set('display_errors', 1); error_reporting(E_ALL);
 
 /* Baseado no código do Yahoo { http://developer.yahoo.com/php/howto-cacheRestPhp.html }
 ** Função utilizada como Cache para evitar atingir o servidor remoto o tempo todo.
--- index.php
+++ index.php
@@ -1,4 +1,5 @@
 <?php
+//ini_set('display_errors', 1); error_reporting(E_ALL);
 
 $executionTime = microtime(true);
 include_once './common.php';
